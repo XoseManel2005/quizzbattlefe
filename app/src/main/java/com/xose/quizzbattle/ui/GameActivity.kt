548822999f06a1c4151a43acb28d959ef17c8f1a
Xose Manel Gonzalez <xm.gonzalez@nextt.es>
2025-04-08 20:36:42 +0200

    Se hizo la pantalla de pregunta

--- app/src/main/java/com/xose/quizzbattle/ui/GameActivity.kt
+++ app/src/main/java/com/xose/quizzbattle/ui/GameActivity.kt
@@ -1,12 +1,22 @@
 package com.xose.quizzbattle.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.xose.quizzbattle.R
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val imgCategory = findViewById<ImageView>(R.id.imgSelectedCategory)
+
+
+        imgCategory.setOnClickListener {
+            val intent = Intent (this, QuestionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
