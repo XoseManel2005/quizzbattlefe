70a1142f2f7f689e1ca5027d44232bb164640ea2
PauEspina33 <pauespinacueto@gmail.com>
2025-04-23 20:29:25 +0200

    Se ha dividido todo tipo de partidas

---
+++ app/src/main/java/com/xose/quizzbattle/ui/GameFragment.kt
@@ -0,0 +1,81 @@
+package com.xose.quizzbattle.ui
+
+import FinishedGameAdapter
+import GameAdapter
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.xose.quizzbattle.R
+import com.xose.quizzbattle.data.ApiClient
+import com.xose.quizzbattle.data.GameService
+import com.xose.quizzbattle.model.Game
+import com.xose.quizzbattle.model.User
+import com.xose.quizzbattle.util.SessionManager
+import kotlinx.coroutines.launch
+
+class GamesFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerViewRival: RecyclerView
+    private lateinit var adapter: GameAdapter
+    private lateinit var gameService: GameService
+    private lateinit var usuarioLogueado: User
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_games, container, false)
+
+        recyclerViewRival = view.findViewById(R.id.rvRivalGames)
+        recyclerViewRival.layoutManager = LinearLayoutManager(requireContext())
+
+        recyclerView = view.findViewById(R.id.rvPartidas)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        usuarioLogueado = SessionManager(requireContext()).getLoggedUser() ?: return view
+        gameService = ApiClient.getGameService(requireContext())
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            var userGames = mutableListOf<Game>()
+            var rivalGames = mutableListOf<Game>()
+            val games = gameService.getGames(usuarioLogueado.username, Game.Status.ONGOING.toString())
+            games.forEach { game ->
+                if (game.turn?.username == usuarioLogueado.username) {
+                    userGames.add(game)
+                } else {
+                    rivalGames.add(game)
+                }
+            }
+            try {
+
+                adapter = GameAdapter(userGames, usuarioLogueado) {
+                    Log.d("LOAD_GAMES", "$it")
+                }
+                recyclerView.adapter = adapter
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(requireContext(), "Error al cargar partidas", Toast.LENGTH_LONG).show()
+            }
+
+            try {
+                adapter = GameAdapter(rivalGames, usuarioLogueado) {
+                    Log.d("LOAD_GAMES", "$it")
+                }
+                recyclerViewRival.adapter = adapter
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(requireContext(), "Error al cargar partidas", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        return view
+    }
+}
--- app/src/main/java/com/xose/quizzbattle/ui/GamesActivity.kt
+++ app/src/main/java/com/xose/quizzbattle/ui/GamesActivity.kt
@@ -2,65 +2,61 @@ package com.xose.quizzbattle.ui
 
 import FinishedGameAdapter
 import GameAdapter
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.xose.quizzbattle.R
-import com.xose.quizzbattle.data.ApiClient
-import com.xose.quizzbattle.data.GameService
-import com.xose.quizzbattle.model.Game
 import com.xose.quizzbattle.model.User
-import com.xose.quizzbattle.util.SessionManager
-import kotlinx.coroutines.launch
 
 class GamesActivity : AppCompatActivity() {
-
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var recyclerViewFinished: RecyclerView
-    private lateinit var adapter: GameAdapter
-    private lateinit var adapterFinished: FinishedGameAdapter
-    private lateinit var gameService: GameService
-    private lateinit var usuarioLogueado: User
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_games)
 
-        recyclerView = findViewById(R.id.rvPartidas)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        val btnOngoing = findViewById<Button>(R.id.btnOngoing)
+        val btnFinished = findViewById<Button>(R.id.btnFinished)
 
-        recyclerViewFinished = findViewById(R.id.rvPartidasAcabadas)
-        recyclerViewFinished.layoutManager = LinearLayoutManager(this)
+// Mostrar fragmento de partidas por defecto
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, GamesFragment())
+            .commit()
 
-        usuarioLogueado = SessionManager(this).getLoggedUser() ?: return
-        gameService = ApiClient.getGameService(this)
 
-        lifecycleScope.launch {
-            try {
-                val games = gameService.getGames(username = usuarioLogueado.username, status = Game.Status.ONGOING.toString())
-                adapter = GameAdapter(games, usuarioLogueado) { selectedGame ->
-                    Log.d("LOAD_GAMES", "${selectedGame.toString()}")
-                }
-                recyclerView.adapter = adapter
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Toast.makeText(this@GamesActivity, "Error al cargar partidas: ${e.message}", Toast.LENGTH_LONG).show()
-                Log.d("LOAD_GAMES", "Error al cargar partidas: ${e.message}")
-            }
+// Colores
+        val colorSelected = Color.parseColor("#56000000") // más oscuro
+        val colorUnselected = Color.parseColor("#00000000") // transparente
 
-            try {
-                val games = gameService.getGames(username = usuarioLogueado.username, status = Game.Status.FINISHED.toString())
-                adapterFinished = FinishedGameAdapter(games, usuarioLogueado)
-                recyclerViewFinished.adapter = adapterFinished
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Toast.makeText(this@GamesActivity, "Error al cargar partidas: ${e.message}", Toast.LENGTH_LONG).show()
-                Log.d("LOAD_GAMES", "Error al cargar partidas: ${e.message}")
-            }
+        // Cambiar colores
+        btnFinished.backgroundTintList = ColorStateList.valueOf(colorSelected)
+        btnOngoing.backgroundTintList = ColorStateList.valueOf(colorUnselected)
+        btnOngoing.isEnabled = false
+
+        btnOngoing.setOnClickListener {
+            btnFinished.isEnabled = true
+            // Cambiar colores
+            btnFinished.backgroundTintList = ColorStateList.valueOf(colorSelected)
+            btnOngoing.backgroundTintList = ColorStateList.valueOf(colorUnselected)
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, GamesFragment())
+                .commit()
         }
+
+        btnFinished.setOnClickListener {
+            btnOngoing.isEnabled = true
+            // Cambiar colores
+            btnOngoing.backgroundTintList = ColorStateList.valueOf(colorSelected)
+            btnFinished.backgroundTintList = ColorStateList.valueOf(colorUnselected)
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, FinishedGameFragment())
+                .commit()
+        }
+
+
+        // Puedes hacer que imgFriendships abra otro fragmento
     }
 }
