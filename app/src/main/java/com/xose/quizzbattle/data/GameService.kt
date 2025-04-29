a7ceff0da216d0c7e301b355cce9b6abb9b40640
PauEspina33 <pauespinacueto@gmail.com>
2025-04-29 20:00:35 +0200

    Se ha hecho la logica de la partida. Falta arreglar que al volver al menu te envia a la partida otra vez

--- app/src/main/java/com/xose/quizzbattle/data/GameService.kt
+++ app/src/main/java/com/xose/quizzbattle/data/GameService.kt
@@ -1,11 +1,17 @@
 package com.xose.quizzbattle.data
 
 import com.xose.quizzbattle.model.Game
+import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface GameService {
     @GET("games/find/all")
     suspend fun getGames(@Query("username") username: String, @Query("status") status: String): List<Game>
+
+    @PUT("games/update")
+    fun updateGame(@Body request: Game): Call<Game>
 }
--- app/src/main/java/com/xose/quizzbattle/ui/QuestionActivity.kt
+++ app/src/main/java/com/xose/quizzbattle/ui/QuestionActivity.kt
@@ -18,13 +18,14 @@ import com.xose.quizzbattle.data.ApiClient
 import com.xose.quizzbattle.model.Category
 import com.xose.quizzbattle.model.Game
 import com.xose.quizzbattle.model.Question
+import com.xose.quizzbattle.model.User
 import com.xose.quizzbattle.util.SessionManager
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class QuestionActivity : AppCompatActivity() {
-
+    private lateinit var usuarioLogueado: User
     private lateinit var tvQuestion: TextView
     private lateinit var imgQuestion: ImageView
     private lateinit var btnAnswer1: Button
@@ -44,6 +45,7 @@ class QuestionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
 
+        usuarioLogueado = (SessionManager(this).getLoggedUser() ?: null) as User
         tvQuestion = findViewById(R.id.tvQuestion)
         imgQuestion = findViewById(R.id.imgQuestion)
         btnAnswer1 = findViewById(R.id.btnAnswer1)
@@ -101,7 +103,11 @@ class QuestionActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         val question = response.body()
                         if (question == null) {
-                            Toast.makeText(this@QuestionActivity, "No hay pregunta disponible", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                this@QuestionActivity,
+                                "No hay pregunta disponible",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         } else {
                             Log.d("QUESTION_RECEIVED", "Pregunta: $question")
 
@@ -139,13 +145,18 @@ class QuestionActivity : AppCompatActivity() {
                         }
                     } else {
                         Log.e("QUESTION_DEBUG", "Error al cargar pregunta: ${response.code()}")
-                        Toast.makeText(this@QuestionActivity, "Error cargando pregunta", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this@QuestionActivity,
+                            "Error cargando pregunta",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
 
                 override fun onFailure(call: Call<Question>, t: Throwable) {
                     Log.e("QUESTION_DEBUG", "Fallo de conexión: ${t.localizedMessage}")
-                    Toast.makeText(this@QuestionActivity, "Fallo de conexión", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@QuestionActivity, "Fallo de conexión", Toast.LENGTH_SHORT)
+                        .show()
                 }
             })
     }
@@ -157,29 +168,150 @@ class QuestionActivity : AppCompatActivity() {
 
         val buttons = listOf(btnAnswer1, btnAnswer2, btnAnswer3, btnAnswer4)
         buttons.forEach { it.setOnClickListener { null } }
-
+        playSound(R.raw.correct_answer)
         if (selectedAnswer == correctAnswer) {
-            playSound(R.raw.correct_answer)
+            // Si la respuesta es correcta
+            if (usuarioLogueado.username == game?.player1?.username) {
+                // Si es el turno del jugador 1
+                game?.scorePlayer1 =
+                    game?.scorePlayer1?.plus(1) ?: 1  // Sumar 1 al score de player 1
+                if (game?.scorePlayer1 == 5) {
+                    // Sumar una estrella
+                    game?.starsPlayer1 = game?.starsPlayer1?.plus(1) ?: 1
+                    // Reiniciar el score
+                    game?.scorePlayer1 = 0
+                    game!!.turn = game!!.player2
+                    if (game?.starsPlayer1 == 3){
+                        if (game?.starsPlayer2!! < 2) {
+                            // Si el jugador 2 no tiene 3 estrellas, el jugador 1 es el ganador
+                            game?.winner = game?.player1!!
+                            game?.status = Game.Status.FINISHED
+                        }
+                    }
+                    updateGame(game!!)
+                    val intent = Intent(this, GamesActivity::class.java)
+                    startActivity(intent)
+                }
 
+
+                // Si el jugador 1 tiene 3 estrellas, termina el turno y pasa al jugador 2
+                if (game?.starsPlayer1 == 3) {
+                    game?.turn = game?.player2
+                    if (game?.starsPlayer2!! < 2) {
+                        // Si el jugador 2 no tiene 3 estrellas, el jugador 1 es el ganador
+                        game?.winner = game?.player1!!
+                    }
+                    updateGame(game!!)
+                    val intent = Intent(this, GamesActivity::class.java)
+                    startActivity(intent)
+                }
+
+                // Si el jugador 1 tiene menos de 3 estrellas, solo avanza al siguiente turno
+                if (game?.starsPlayer1 != 3) {
+                    val intent = Intent(this, CategoryActivity::class.java)
+                    intent.putExtra("SELECTED_GAME", game)
+                    startActivity(intent)
+                }
+
+            } else {
+                // Si es el turno del jugador 2
+                game?.scorePlayer2 =
+                    game?.scorePlayer2?.plus(1) ?: 1  // Sumar 1 al score de player 2
+                if (game?.scorePlayer2 == 5) {
+                    // Sumar una estrella cuando el jugador 2 llegue a 5 puntos
+                    game?.starsPlayer2 = game?.starsPlayer2?.plus(1) ?: 1
+                }
+
+                // Si el jugador 2 tiene 3 estrellas, termina el turno y el juego
+                if (game?.starsPlayer2 == 3) {
+                    game?.turn = null
+                    if (game?.starsPlayer1 != 3) {
+                        // Si el jugador 1 no tiene 3 estrellas, el jugador 2 es el ganador
+                        game?.winner = game?.player2!!
+                    }
+                    updateGame(game!!)
+                    val intent = Intent(this, GamesActivity::class.java)
+                    startActivity(intent)
+                }
+
+                // Si el jugador 2 tiene menos de 3 estrellas, solo avanza al siguiente turno
+                if (game?.starsPlayer2 != 3) {
+                    val intent = Intent(this, CategoryActivity::class.java)
+                    intent.putExtra("SELECTED_GAME", game)
+                    startActivity(intent)
+                }
+            }
+
+            // Continuar con el cambio de turno después de un retraso
             handler.postDelayed({
                 val intent = Intent(this, CategoryActivity::class.java)
                 intent.putExtra("SELECTED_GAME", game)
                 startActivity(intent)
             }, 2000)
+
         } else {
+            // Si la respuesta es incorrecta
             playSound(R.raw.wrong_answer)
 
             handler.postDelayed({
-                val intent = Intent(this, GamesActivity::class.java)
-                startActivity(intent)
+                // Verificar si el jugador 1 ha ganado
+                if (game?.starsPlayer1 == 3) {
+                    game?.winner = game?.player1!!
+                    updateGame(game!!)  // Actualizar el estado del juego
+
+                    // Iniciar pantalla de resultados con la victoria del jugador 1
+                    val intent = Intent(this, GamesActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    if (usuarioLogueado.username == game?.player1?.username) {
+                        game!!.turn = game!!.player2
+                    } else {
+                        game!!.turn = game!!.player1
+                    }
+                    updateGame(game!!)
+                    // Si no ha ganado, continuar con el turno
+                    val intent = Intent(this, GamesActivity::class.java)
+                    startActivity(intent)
+                }
             }, 2000)
         }
+
+
     }
 
     private fun playSound(resId: Int) {
         mediaPlayer?.release()
+
         mediaPlayer = MediaPlayer.create(this, resId)
         mediaPlayer?.start()
+    }
+
+    private fun updateGame(game: Game) {
+        val api = ApiClient.getGameService(this)
+
+        Log.d("CATEGORY_ID_DEBUG", "Haciendo update del game: $game")
+
+        api.updateGame(game).enqueue(object : Callback<Game> {
+            override fun onResponse(call: Call<Game>, response: Response<Game>) {
+                if (response.isSuccessful) {
+                    Log.d("CATEGORY_ID_DEBUG", "Update hecho correctamente: $game")
+                    // Puedes hacer algo más aquí, como actualizar UI o notificar al usuario.
+                } else {
+                    val errorBody = response.errorBody()?.string()
+                    Log.e("QUESTION_DEBUG", "Error al actualizar el juego: ${response.code()} - ${errorBody}")
+                    Toast.makeText(
+                        this@QuestionActivity,
+                        "Error actualizando el juego",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Game>, t: Throwable) {
+                Log.e("QUESTION_DEBUG", "Fallo de conexión: ${t.localizedMessage}")
+                Toast.makeText(this@QuestionActivity, "Fallo de conexión", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     override fun onDestroy() {
